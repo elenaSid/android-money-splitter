ab7f45493d99ea92332acdf12e3383a23f2735ee
elenaSid <elena.sid1991@gmail.com>
2019-01-04 18:19:08 +0700

    Added adapter for users screen. Created popup menu for users list

--- app/src/main/java/com/elena/moneysplitter/users/ui/UserFragment.kt
+++ app/src/main/java/com/elena/moneysplitter/users/ui/UserFragment.kt
@@ -3,9 +3,13 @@ package com.elena.moneysplitter.users.ui
 import android.content.Context
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupWindow
+import android.widget.TextView
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
@@ -13,6 +17,7 @@ import com.elena.moneysplitter.R
 import com.elena.moneysplitter.databinding.UsersFragmentBinding
 import com.elena.moneysplitter.users.mvp.UsersPresenter
 import com.elena.moneysplitter.users.mvp.UsersView
+import com.elena.moneysplitter.utils.DisplayUtils
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
@@ -21,7 +26,8 @@ import javax.inject.Inject
  *         Date: 03/01/2019
  *         Time: 23:03
  */
-class UserFragment : MvpAppCompatFragment(), UsersView {
+class UserFragment : MvpAppCompatFragment(), UsersView, UserAdapter.UserListener {
+
     private lateinit var binding: UsersFragmentBinding
 
     @Inject
@@ -32,6 +38,7 @@ class UserFragment : MvpAppCompatFragment(), UsersView {
     fun provideUsersPresenter(): UsersPresenter {
         return presenter
     }
+
 
     override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
@@ -46,5 +53,41 @@ class UserFragment : MvpAppCompatFragment(), UsersView {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         binding.fabAddUser.setOnClickListener { presenter.onUserAddClicked() }
+        initUsersList()
+    }
+
+    private fun initUsersList() {
+        val adapter = UserAdapter(this)
+        val users = ArrayList<Pair<String, String>>()
+        users.add(Pair("West", "West's"))
+        users.add(Pair("Elena", "West's"))
+        adapter.setUsers(users)
+
+        binding.rvUsers.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.rvUsers.adapter = adapter
+    }
+
+    override fun onMoreClicked(anchor: View, user: Pair<String, String>) {
+        val popupWindow = PopupWindow(context)
+        val popupContent = View.inflate(context, R.layout.menu_view, null)
+        (popupContent.findViewById(R.id.btnEdit) as TextView).setOnClickListener {
+            //TODO: вызвать экран редактирования
+            popupWindow.dismiss()
+        }
+        (popupContent.findViewById(R.id.btnDelete) as TextView).setOnClickListener {
+            //TODO:Удалять пользователей
+            popupWindow.dismiss()
+        }
+        popupWindow.contentView = popupContent
+        popupWindow.width = ViewGroup.LayoutParams.WRAP_CONTENT
+        popupWindow.height = ViewGroup.LayoutParams.WRAP_CONTENT
+        popupWindow.contentView = popupContent
+        popupWindow.isOutsideTouchable = true
+
+        val anchorScreenPosition = IntArray(2)
+        anchor.getLocationInWindow(anchorScreenPosition)
+        val yOffset = anchorScreenPosition[1] - DisplayUtils.dpToPx(resources, 48F)
+        popupWindow.showAtLocation(anchor, Gravity.END or Gravity.TOP,
+                DisplayUtils.dpToPx(resources, 19F), yOffset)
     }
 }
