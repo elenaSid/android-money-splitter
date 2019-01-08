507b8500c6fa826b4279d5154405ffb145361de8
elenaSid <elena.sid1991@gmail.com>
2019-01-08 16:22:34 +0700

    Added adapter for items

---
+++ app/src/main/java/com/elena/moneysplitter/items/list/ui/ItemAdapter.kt
@@ -0,0 +1,59 @@
+package com.elena.moneysplitter.items.list.ui
+
+import android.support.annotation.IntDef
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.elena.moneysplitter.R
+
+/**
+ * @author elena
+ *         Date: 08/01/2019
+ *         Time: 15:48
+ */
+class ItemAdapter(private val listener: ItemListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        @IntDef(TYPE_HEADER, TYPE_ITEM)
+        @Retention(AnnotationRetention.SOURCE)
+        annotation class ViewType
+
+        const val TYPE_HEADER = 0
+        const val TYPE_ITEM = 1
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == 0) {
+            return ItemAdapter.TYPE_HEADER
+        }
+        return ItemAdapter.TYPE_ITEM
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+                if (viewType == TYPE_HEADER) R.layout.item_header_items else R.layout.item_shopping_item, null)
+        view.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT)
+        return object : RecyclerView.ViewHolder(view) {}
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+
+
+    interface ItemListener {
+
+        //TODO: Передавать item
+        fun onMoreClicked(anchor: View)
+
+        fun onItemClicked()
+    }
+}
--- app/src/main/java/com/elena/moneysplitter/items/list/ui/ItemFragment.kt
+++ app/src/main/java/com/elena/moneysplitter/items/list/ui/ItemFragment.kt
@@ -2,6 +2,7 @@ package com.elena.moneysplitter.items.list.ui
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,12 +15,29 @@ import com.elena.moneysplitter.databinding.ItemsFragmentBinding
  *         Date: 08/01/2019
  *         Time: 15:29
  */
-class ItemFragment : MvpAppCompatFragment() {
+class ItemFragment : MvpAppCompatFragment(), ItemAdapter.ItemListener {
 
     private lateinit var binding: ItemsFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fr_items, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initList()
+    }
+
+    private fun initList() {
+        val adapter = ItemAdapter(this)
+        binding.rvItems.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.rvItems.adapter = adapter
+    }
+
+    override fun onMoreClicked(anchor: View) {
+    }
+
+    override fun onItemClicked() {
     }
 }
