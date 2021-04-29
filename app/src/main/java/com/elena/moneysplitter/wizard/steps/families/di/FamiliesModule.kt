42bd14848cbd33875fa78c60db232d0ac2a599ae
elenaSid <elena.sid1991@gmail.com>
2021-04-29 23:15:37 +0700

    Добавлено отображение пустого списка семей

---
+++ app/src/main/java/com/elena/moneysplitter/wizard/steps/families/di/FamiliesModule.kt
@@ -0,0 +1,7 @@
+package com.elena.moneysplitter.wizard.steps.families.di
+
+/**
+ * @author elena
+ */
+class FamiliesModule {
+}
---
+++ app/src/main/java/com/elena/moneysplitter/wizard/steps/families/mvp/FamiliesPresenter.kt
@@ -0,0 +1,7 @@
+package com.elena.moneysplitter.wizard.steps.families.mvp
+
+/**
+ * @author elena
+ */
+class FamiliesPresenter {
+}
